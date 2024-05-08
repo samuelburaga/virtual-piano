3e117ba2f6f7b424434b2723d729889c110c3d7b
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2024-05-08 11:20:09 +0300

    feat: implement basic message receiver

---
+++ receiver.py
@@ -0,0 +1,24 @@
+from utils.rabbit_mq_utils import RabbitMQConnection
+from constants.constants import RabbitMQQueues
+from utils.webcam_utils import detect_number_of_webcams
+from app import start
+
+
+def receive(configuration):
+    rabbit_mq_connection = RabbitMQConnection(configuration["rabbit-mq"]["host"])
+    rabbit_mq_connection.declare_queue(queue=RabbitMQQueues.VIRTUAL_PIANO.value)
+
+    def message_received_callback(ch, method, properties, message_body):
+        print(f"A message has been received! Message: {message_body}")
+        if message_body == b"start":
+            user_webcams_count = detect_number_of_webcams()
+            start(user_webcams_count)
+
+    rabbit_mq_connection.channel.basic_consume(
+        queue=RabbitMQQueues.VIRTUAL_PIANO.value,
+        on_message_callback=message_received_callback,
+        auto_ack=True,
+    )
+
+    print("Waiting for messages...")
+    rabbit_mq_connection.channel.start_consuming()
